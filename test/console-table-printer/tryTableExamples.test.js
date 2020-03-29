e288a71ddc1d4882bfeb3e010ac565ca33725f0f
Nahiyan Kamal <noyanayihan@gmail.com>
2020-03-30 00:26:24 +0200

    adding new tests

---
+++ test/console-table-printer/tryTableExamples.test.js
@@ -0,0 +1,21 @@
+const { printTable, Table } = require('console-table-printer');
+
+describe('Example: Print a simple Table without table instance creation', () => {
+  it(`complex tables`, function() {
+    //Create a table
+    const t = new Table({
+      style: 'fatBorder', //style of border of the table
+      columns: [
+        { name: 'column1', alignment: 'left', color: 'red' }, //with alignment and color
+        { name: 'column2', alignment: 'right' },
+        { name: 'column3' },
+      ],
+    });
+
+    t.addRow({ column1: 1, column2: 'red wine', column3: 10.212 });
+    t.addRow({ column1: 2, column2: 'red wine', column3: 10.212 });
+    t.addRow({ column1: 3, column2: 'red wine', column3: 10.212 });
+
+    t.printTable();
+  });
+});
