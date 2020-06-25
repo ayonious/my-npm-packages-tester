ddcf39ef50e596cee701dd23e58ac08dd26e7382
Nahiyan Kamal <noyanayihan@gmail.com>
2020-06-25 16:34:06 +0200

    auto generated message

--- src/aga.js
+++ src/aga.js
@@ -1,23 +1,83 @@
 const { printTable, Table } = require('console-table-printer');
 
+const data = [
+  {
+    date: '2020-05-20',
+    mismatches: 209,
+    matches: 245,
+  },
+  {
+    date: '2020-05-21',
+    mismatches: 187,
+    matches: 243,
+  },
+  {
+    date: '2020-05-22',
+    mismatches: 216,
+    matches: 224,
+  },
+  {
+    date: '2020-05-23',
+    mismatches: 145,
+    matches: 205,
+  },
+  {
+    date: '2020-05-24',
+    mismatches: 171,
+    matches: 210,
+  },
+  {
+    date: '2020-05-25',
+    mismatches: 259,
+    matches: 329,
+  },
+  {
+    date: '2020-05-26',
+    mismatches: 205,
+    matches: 276,
+  },
+  {
+    date: '2020-05-27',
+    mismatches: 8112,
+    matches: 9052,
+  },
+  {
+    date: '2020-05-28',
+    mismatches: 1454,
+    matches: 1773,
+  },
+  {
+    date: '2020-05-29',
+    mismatches: 1601,
+    matches: 1807,
+  },
+  {
+    date: '2020-05-30',
+    mismatches: 1345,
+    matches: 1642,
+  },
+  {
+    date: '2020-05-31',
+    mismatches: 1361,
+    matches: 1604,
+  },
+  {
+    date: '2020-06-01',
+    mismatches: 1690,
+    matches: 1914,
+  },
+  {
+    date: '2020-06-02',
+    mismatches: 334,
+    matches: 419,
+  },
+];
+
 const t = new Table({
-  title: 'some shit',
   style: 'fatBorder', //style of border of the table, (optional)default = thinBorder
-  columns: [
-    { name: 'column1', alignment: 'left', color: 'red' }, //with alignment and color
-    { name: 'column2', alignment: 'right' },
-    { name: 'column3' },
-  ],
-  sort: (row1, row2) => row2.column1 - row1.column1, // sorting order of rows (optional)
-  filter: (row) => row.column1 < 3, // filtering rows (optional)
+  enabledColumns: ['mismatches', 'date'],
 });
 
-t.addRow({ column1: 1, column2: 'red wine', column3: 0.212 });
-t.addRow({ column1: 2, column2: 'very very red wine', column3: 12.12 });
-t.addRow({
-  column1: 3,
-  column2: 'red wine and some stuffs',
-  column3: 10.212,
-});
+t.addRows(data);
 
 t.printTable();
