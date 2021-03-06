2618b5d4acc68435412136e546398edcc426195f
Nahiyan Kamal <noyanayihan@gmail.com>
2021-03-06 17:07:43 +0100

    fix: update packages

--- test/console-table-printer/calculateColumns.test.js
+++ test/console-table-printer/calculateColumns.test.js
@@ -1,50 +1,55 @@
-const { printTable, Table } = require('console-table-printer');
+const { Table } = require('console-table-printer');
 const chalk = require('chalk');
 
-const p = new Table({
-  columns: [
-    { name: "red_amount", color: "red" },
-    { name: "blue_amount", color: "blue" },
-    { name: "amount", color: "blue" },
-  ],
-  computedColumns: [
-    // creating new columns based on other column vals
+describe('calculateColumns.test', () => {
+  it(`Simple Test`, function () {
+
+    const p = new Table({
+      columns: [
+        { name: "red_amount", color: "red" },
+        { name: "blue_amount", color: "blue" },
+        { name: "amount", color: "blue" },
+      ],
+      computedColumns: [
+        // creating new columns based on other column vals
+        {
+          name: "sum",
+          function: (row) => row.red_amount + row.blue_amount,
+        },
+        {
+          name: "red_percent",
+          function: (row) => { 
+            const val = ((row.red_amount / row.sum) * 100).toFixed(2);
+            if(val <= 50) {
+              return chalk.red(val);
+            }
+            return chalk.blue(val);
+          },
+        },
+      ],
+    });
+
+    // add rows
+    p.addRows([
     {
-      name: "sum",
-      function: (row) => row.red_amount + row.blue_amount,
+      red_amount: 12,
+      blue_amount: 40,
     },
     {
-      name: "red_percent",
-      function: (row) => { 
-        const val = ((row.red_amount / row.sum) * 100).toFixed(2);
-        if(val <= 50) {
-          return chalk.red(val);
-        }
-        return chalk.blue(val);
-      },
+      red_amount: 22,
+      blue_amount: 7,
     },
-  ],
+    {
+      red_amount: 90,
+      blue_amount: 10,
+    },
+    {
+      red_amount: 1,
+      blue_amount: 10,
+    },
+    ]);
+
+    // print
+    p.printTable();  
+  })
 });
-
-// add rows
-p.addRows([
-  {
-    red_amount: 12,
-    blue_amount: 40,
-  },
-  {
-    red_amount: 22,
-    blue_amount: 7,
-  },
-  {
-    red_amount: 90,
-    blue_amount: 10,
-  },
-  {
-    red_amount: 1,
-    blue_amount: 10,
-  },
-]);
-
-// print
-p.printTable();
