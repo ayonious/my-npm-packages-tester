7759951ae9a9a2b899d897756778321d052b77af
Nahiyan Kamal <noyanayihan@gmail.com>
2021-05-14 11:33:48 +0200

    fix(technical): update deps

---
+++ src/limitlin2.js
@@ -0,0 +1,46 @@
+const { Table } = require("console-table-printer");
+
+const p = new Table({
+  columns: [
+    { name: "Index", alignment: "left", color: "red", minLen: 15, title: 'minLen15' },
+    { name: "right_align_text", alignment: "right", maxLen: 15, title: 'maxLen15' },
+    { name: "green", alignment: "center", color: "green", minLen: 20, title: 'minLen20' },
+  ],
+});
+
+// add rows with color
+p.addRow(
+  {
+    Index: 2,
+    right_align_text: "This row is blue",
+    green: 10.212,
+  },
+  { color: "blue" }
+);
+p.addRow(
+  {
+    Index: 3,
+    right_align_text: "I would like some red wine please",
+    green: 10.212,
+  },
+  { color: "red" }
+);
+p.addRow(
+  {
+    Index: 4,
+    right_align_text: "I would like some cyan wine please",
+    green: 10.212,
+  },
+  { color: "cyan" }
+);
+p.addRow(
+  {
+    Index: 8,
+    right_align_text: "I would like some gelb bananen bitte",
+    green: 100,
+  },
+  { color: "yellow" }
+);
+
+// print
+p.printTable();
